62aa5f32ea136b29e18554ad7998ed51c696d400
Ghassan Aljaafri <ghassan.aljaafri@gmail.com>
2021-01-15 21:20:19 +0200

    User Interface Completed

--- Visual Programming - HW1 Employee System/Form1.Designer.cs
+++ Visual Programming - HW1 Employee System/Form1.Designer.cs
@@ -29,13 +29,204 @@ namespace Visual_Programming___HW1_Employee_System
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.labelSsn = new System.Windows.Forms.Label();
+            this.textBoxSsn = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.dateTimePickerBirthDay = new System.Windows.Forms.DateTimePicker();
+            this.labelBirthDay = new System.Windows.Forms.Label();
+            this.buttonAddEmployee = new System.Windows.Forms.Button();
+            this.labelDepartment = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(12, 29);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(200, 20);
+            this.textBoxFirstName.TabIndex = 0;
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Location = new System.Drawing.Point(13, 13);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(57, 13);
+            this.labelFirstName.TabIndex = 1;
+            this.labelFirstName.Text = "First Name";
+            this.labelFirstName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelSsn
+            // 
+            this.labelSsn.AutoSize = true;
+            this.labelSsn.Location = new System.Drawing.Point(12, 95);
+            this.labelSsn.Name = "labelSsn";
+            this.labelSsn.Size = new System.Drawing.Size(29, 13);
+            this.labelSsn.TabIndex = 2;
+            this.labelSsn.Text = "SSN";
+            // 
+            // textBoxSsn
+            // 
+            this.textBoxSsn.Location = new System.Drawing.Point(12, 112);
+            this.textBoxSsn.Name = "textBoxSsn";
+            this.textBoxSsn.Size = new System.Drawing.Size(200, 20);
+            this.textBoxSsn.TabIndex = 3;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(12, 72);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(200, 20);
+            this.textBoxLastName.TabIndex = 5;
+            this.textBoxLastName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(13, 56);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(58, 13);
+            this.labelLastName.TabIndex = 4;
+            this.labelLastName.Text = "Last Name";
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(12, 151);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(200, 20);
+            this.textBoxAddress.TabIndex = 7;
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Location = new System.Drawing.Point(13, 135);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(45, 13);
+            this.labelAddress.TabIndex = 6;
+            this.labelAddress.Text = "Address";
+            // 
+            // dateTimePickerBirthDay
+            // 
+            this.dateTimePickerBirthDay.Location = new System.Drawing.Point(12, 194);
+            this.dateTimePickerBirthDay.Name = "dateTimePickerBirthDay";
+            this.dateTimePickerBirthDay.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerBirthDay.TabIndex = 8;
+            // 
+            // labelBirthDay
+            // 
+            this.labelBirthDay.AutoSize = true;
+            this.labelBirthDay.Location = new System.Drawing.Point(12, 178);
+            this.labelBirthDay.Name = "labelBirthDay";
+            this.labelBirthDay.Size = new System.Drawing.Size(47, 13);
+            this.labelBirthDay.TabIndex = 9;
+            this.labelBirthDay.Text = "BirthDay";
+            // 
+            // buttonAddEmployee
+            // 
+            this.buttonAddEmployee.Location = new System.Drawing.Point(12, 264);
+            this.buttonAddEmployee.Name = "buttonAddEmployee";
+            this.buttonAddEmployee.Size = new System.Drawing.Size(200, 23);
+            this.buttonAddEmployee.TabIndex = 10;
+            this.buttonAddEmployee.Text = "Add Employee";
+            this.buttonAddEmployee.UseVisualStyleBackColor = true;
+            // 
+            // labelDepartment
+            // 
+            this.labelDepartment.AutoSize = true;
+            this.labelDepartment.Location = new System.Drawing.Point(12, 221);
+            this.labelDepartment.Name = "labelDepartment";
+            this.labelDepartment.Size = new System.Drawing.Size(62, 13);
+            this.labelDepartment.TabIndex = 11;
+            this.labelDepartment.Text = "Department";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 237);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 21);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(219, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(569, 231);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(219, 29);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(488, 20);
+            this.textBoxSearch.TabIndex = 14;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(713, 26);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 15;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 296);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelDepartment);
+            this.Controls.Add(this.buttonAddEmployee);
+            this.Controls.Add(this.labelBirthDay);
+            this.Controls.Add(this.dateTimePickerBirthDay);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.textBoxSsn);
+            this.Controls.Add(this.labelSsn);
+            this.Controls.Add(this.labelFirstName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.Label labelSsn;
+        private System.Windows.Forms.TextBox textBoxSsn;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBirthDay;
+        private System.Windows.Forms.Label labelBirthDay;
+        private System.Windows.Forms.Button buttonAddEmployee;
+        private System.Windows.Forms.Label labelDepartment;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonSearch;
     }
 }
 
--- Visual Programming - HW1 Employee System/Form1.cs
+++ Visual Programming - HW1 Employee System/Form1.cs
@@ -16,5 +16,15 @@ namespace Visual_Programming___HW1_Employee_System
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
